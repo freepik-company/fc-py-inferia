97df9bc6ba79824da2af5fc20f90ae1bfaec5e6f
jamuriano <jamuriano@freepik.com>
2025-01-20 13:02:00 +0100

    refactor(setup.py): update project description and remove old one
    - Revise project description for clarity and conciseness
    - Eliminate outdated long_description string
    - Add new, detailed long_description with key features and benefits of Cogito

--- setup.py
+++ setup.py
@@ -6,7 +6,18 @@ name = "cogito"
 version = "0.0.0"
 python_requires = ">= 3.10"
 description = "Cogito is a Python module designed to streamline the creation and deployment of inference services. It provides tools and abstractions to wrap machine learning models and expose them as robust, production-ready APIs with minimal effort."
-long_description = """A longer description of the project."""
+long_description = """
+Cogito is a versatile Python module aimed at simplifying the development and deployment of inference services. 
+It allows users to wrap machine learning models or any computational logic into APIs effortlessly. 
+With Cogito, you can focus on your core algorithmic functionality while the module takes care of the heavy lifting, including API structure, request handling, error management, and scalability.
+
+Key features include:
+	•	Ease of Use: Simplifies the process of converting your models into production-ready APIs with minimal boilerplate code.
+	•	Customizable API: Provides flexibility to define endpoints, input/output formats, and pre/post-processing logic.
+	•	Scalability: Optimized to handle high-throughput scenarios with support for modern server frameworks.
+	•	Extensibility: Easily integrate with third-party libraries, monitoring tools, or cloud services.
+	•	Error Handling: Built-in mechanisms to catch and handle runtime issues gracefully.
+"""
 author = "freepik-company/tech-avengers"
 author_email = "aivengers@freepik.com"
 # Get the requirements from the requirements.txt file
