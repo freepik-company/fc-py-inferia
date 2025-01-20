4a5e73edcbd4f84d2db0617912862a7c356d6480
jamuriano <jamuriano@freepik.com>
2025-01-20 15:09:57 +0100

    refactor(cogito/core): update application initialization and routing setup
    - Add debug mode option to FastAPI app
    - Include default health check route
    - Implement custom routes for model predictions
    - Remove unused code snippet

--- cogito/core/app.py
+++ cogito/core/app.py
@@ -1,6 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 
+from cogito.api.handlers import create_model_handler, health_check_handler
+
 
 class Application:
     def __init__(
@@ -11,19 +13,46 @@ class Application:
             host: str = "127.0.0.1",
             port: int = 8000,
             show_fastapi_access_logs: bool = True,
+            debug_mode: bool = False,
     ):
 
         self.host = host
         self.port = port
+        self.debug_mode = debug_mode
 
         self.app = FastAPI(
             title=title,
             version=version,
             description=description,
             access_log=show_fastapi_access_logs,
+            debug=debug_mode,
         )
 
-        #self.app.include_router(router)
+        """ Include default routes """
+        self.app.add_api_route(
+                "/health-check",
+                health_check_handler,
+                methods=["GET"],
+                name="health_check",
+                description="Health check endpoint",
+                tags=["health"],
+        )
+
+        """ Include custom routes """
+        routes = [
+            {'path': '/v1/predict/text2image', 'predictor_class': "text2image"},
+            {'path': '/v1/predict/image2image', 'predictor_class': "image2image"},
+        ] # fixme Add real routes here
+
+        for route in routes:
+            self.app.add_api_route(
+                    route['path'],
+                    create_model_handler(route['predictor_class']),
+                    methods=["POST"],
+                    name=route['model'],
+                    description=f"Predict using the {route['predictor_class']} predictor class",
+                    tags=[route['model']],
+            )
 
     def run(self):
         uvicorn.run(self.app, host=self.host, port=self.port)
