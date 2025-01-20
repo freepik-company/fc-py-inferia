62c0e05acecd7f2377cde925ee77344513c4a064
jamuriano <jamuriano@freepik.com>
2025-01-20 14:19:18 +0100

    refactor(cogito/core/app.py): Simplify app initialization by removing redundant instance variables and integrating them into the FastAPI constructor
    - Remove unnecessary self.title, self.version, self.description instance variables
    - Add access_log parameter to FastAPI constructor for show_fastapi_access_logs functionality

--- cogito/core/app.py
+++ cogito/core/app.py
@@ -10,19 +10,17 @@ class Application:
             description: str = "An API for inference and reasoning with an amazing user interface",
             host: str = "127.0.0.1",
             port: int = 8000,
+            show_fastapi_access_logs: bool = True,
     ):
-        self.title = title
-        self.version = version
-        self.description = description
+
         self.host = host
         self.port = port
 
         self.app = FastAPI(
-            title=self.title,
-            version=self.version,
-            description=self.description,
-            docs_url="/",
-            redoc_url=None,
+            title=title,
+            version=version,
+            description=description,
+            access_log=show_fastapi_access_logs,
         )
 
         #self.app.include_router(router)
