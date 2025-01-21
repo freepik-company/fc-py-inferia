9c50ef9db7af5381d5351a715feac7b4d69b6b74
Sergio Soto <ssoto@freepik.com>
2025-01-21 12:47:40 +0100

    fix(config.py): Fixes bug in load_from_file exception handling (#16)

--- cogito/core/config.py
+++ cogito/core/config.py
@@ -26,12 +26,9 @@ class ConfigFile(BaseModel):
             with open(file_path, "r") as file:
                 yaml_data = yaml.safe_load(file)
             return cls(**yaml_data)
-        except FileNotFoundError as e:
-            logging.info(f"Config file not found: {file_path}. Empty config will be used. {e}")
         except Exception as e:
-            logging.error(f"Error loading config file: {file_path}. Empty config will be used. {e}")
-        finally:
-            logging.debug(f"Starting up with empty config.")
+            logging.error(f"Error loading config file: {file_path}: {e}")
+            logging.error(f"Starting up with empty config.")
             return cls(
                     name="Cogito ergo sum",
                     description="An API for inference and reasoning with an amazing user interface",
