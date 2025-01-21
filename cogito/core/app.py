9e0cb07853424b10ca9ec81d173f163764cc0f5a
jamuriano <jamuriano@freepik.com>
2025-01-21 11:16:30 +0100

    refactor(cogito/core/app.py): Refactor application initialization and routing for FastAPI integration
    - Remove old handlers and import necessary modules
    - Update Application class to handle new FastAPI features
    - Initialize map_route_to_model and map_model_to_instance dictionaries
    - Load custom routes from configuration file and set up predictors
    - Add new create_predictor_handler function for routing
    - Handle None when getting predictor instance from map_model_to_instance

--- cogito/core/app.py
+++ cogito/core/app.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+from typing import Dict
 
 import uvicorn
 from fastapi import FastAPI
 
-from cogito.api.handlers import create_model_handler, health_check_handler
+from cogito.core.models import BasePredictor
+from cogito.api.handlers import create_predictor_handler, health_check_handler
 from cogito.core.config import ConfigFile
+from cogito.core.utils import load_predictor
 
 
 class Application:
@@ -22,6 +25,9 @@ class Application:
         self.host = host
         self.port = port
         self.debug_mode = debug_mode
+
+        map_route_to_model: Dict[str, str] = {}
+        map_model_to_instance: Dict[str, BasePredictor] = {}
 
         self.app = FastAPI(
             title=title,
@@ -46,9 +52,20 @@ class Application:
         config = ConfigFile.load_from_file(f"{Path.cwd()}/cogito.yaml")
 
         for route in config.routes:
+            map_route_to_model[route.path] = route.predictor
+            if route.predictor not in map_model_to_instance:
+                predictor = load_predictor(route.predictor)
+                try:
+                    predictor.setup()
+                except Exception as e:
+                    print(f"Error setting up predictor {route.predictor}: {e}")
+                    raise #fixme Use a custom exception
+
+                map_model_to_instance[route.predictor] = predictor
+
             self.app.add_api_route(
                     route.path,
-                    create_model_handler(route.predictor),
+                    create_predictor_handler(map_model_to_instance.get(route.predictor)), #fixme Handle None
                     methods=["POST"],
                     name=route.name,
                     description=route.description,
