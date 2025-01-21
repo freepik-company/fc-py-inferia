9a4ba3617e282f22ed519afa19d1827e1b1f3203
jamuriano <jamuriano@freepik.com>
2025-01-21 11:39:11 +0100

    refactor(cogito/core/config.py): Rename and restructure Config classes for better organization
    - Renamed ConfigRoute to RouteConfig
    - Added RouteConfig default method
    - Removed ConfigFile class
    - Added ServerConfig class
    - Updated ServerConfig structure
    - Moved CogitoConfig initialization to default method
    - Updated load_from_file method to return default configuration on error
    - Added save_to_file method for saving configuration to file

--- cogito/core/config.py
+++ cogito/core/config.py
@@ -4,20 +4,75 @@ import yaml
 from pydantic import BaseModel
 from pydantic.v1 import BaseSettings
 
-
-class ConfigRoute(BaseModel):
+class RouteConfig(BaseModel):
+    """
+    Route configuration.
+    """
     name: str
     description: Optional[str] = None
     path: str
     predictor: str
     tags: List[str] = List
 
+    @classmethod
+    def default(cls):
+        return cls(
+                name='Predict',
+                description='Make a single prediction',
+                path='/v1/predict',
+                predictor='predict:Predictor',
+                tags=['predict']
+        )
 
-class ConfigFile(BaseModel):
+class ServerConfig(BaseModel):
+    """
+    Server configuration.
+    """
     name: str
     description: Optional[str] = None
     version: Optional[str] = '1.0.0'
-    routes: List[ConfigRoute] = List
+    routes: List[RouteConfig] = List
+
+    @classmethod
+    def default(cls):
+        return cls(
+                name='Cogito ergo infero',
+                description='Inference server',
+                version='0.1.0',
+                routes=[RouteConfig.default()]
+        )
+
+class TrainingConfig(BaseModel):
+    """
+    Training configuration.
+    """
+    pass
+
+    @classmethod
+    def default(cls):
+        return cls()
+
+
+class CogitoConfig(BaseModel):
+    """
+    Cogito configuration.
+    """
+    server: ServerConfig
+    training: TrainingConfig
+
+    @classmethod
+    def default(cls):
+        return cls(server=ServerConfig.default(), training=TrainingConfig.default())
+
+class ConfigFile(BaseModel):
+    """
+    Configuration file.
+    """
+    cogito: CogitoConfig
+
+    @classmethod
+    def default(cls):
+        return cls(cogito=CogitoConfig.default())
 
     @classmethod
     def load_from_file(cls, file_path: str) -> "ConfigFile":
@@ -26,12 +81,7 @@ class ConfigFile(BaseModel):
                 yaml_data = yaml.safe_load(file)
             return cls(**yaml_data)
         except Exception:
-            return cls(
-                    name="Cogito ergo sum",
-                    description="An API for inference and reasoning with an amazing user interface",
-                    routes=[],
-                    version="1.0.0",
-            )
+            return cls.default()
         
     def save_to_file(self, file_path: str) -> None:
         with open(file_path, "w") as file:
