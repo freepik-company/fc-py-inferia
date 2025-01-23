a705b9bb5a159ca661566ecba9373cd8affa31a9
dieloren <tdiegoldc@gmail.com>
2025-01-23 14:45:09 +0100

    feat(SetupError): added new exception

--- cogito/core/exceptions.py
+++ cogito/core/exceptions.py
@@ -4,3 +4,7 @@ from os import PathLike
 class ConfigFileNotFoundError(Exception):
     def __init__(self, file_path: str):
         super().__init__(f"Config file not found: {file_path}")
+
+class SetupError(Exception):
+    def __init__(self, predictor: str, error: Exception):
+        super().__init__(f"Unable to setup predictor {predictor}: {error}")
