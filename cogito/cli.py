529d3186199c5ba3e5ea560eaad0562791d48fad
Sergio Tejón <sergio.tejon@gmail.com>
2025-01-23 18:05:22 +0100

    refactor(cogito/cli.py): Consolidate 'init' and 'scaffold' commands under 'cli' group
    - Import scaffold command
    - Add a configuration path option to the CLI
    - Update 'add_command' function calls to accommodate new command
    - Modify main script execution to accept an optional context object

--- cogito/cli.py
+++ cogito/cli.py
@@ -1,17 +1,21 @@
 import click
 
 from cogito.commands.initialize import init
-
+from cogito.commands.scaffold_predict import scaffold
 
 @click.group()
-def cli():
+@click.option("-c", "--config-path", type=str, default=".", help="The path to the configuration file")
+@click.pass_context
+def cli(ctx, config_path: str = ".") -> None:
     """
-Cogito CLI
+    Cogito CLI
     """
-    pass
+    ctx.ensure_object(dict)
+    ctx.obj['config_path'] = config_path
+
 
 cli.add_command(init)
-
+cli.add_command(scaffold)
 
 if __name__ == "__main__":
-    cli()
+    cli(obj={})
