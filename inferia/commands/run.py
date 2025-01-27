226151ce43bc98aff550fb8362f10820f7cbf546
Sergio Soto Nuñez <ssoto@freepik.com>
2025-01-27 13:36:41 +0100

    fix: changed the run command logic

--- inferia/commands/run.py
+++ inferia/commands/run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import click
 
@@ -12,11 +13,14 @@ def run(ctx):
     """Run cogito app"""
     config_path = ctx.get("config_path")
     absolute_path = os.path.abspath(config_path)
-    click.echo(f"Running '{absolute_path}' cogito application...")
+    click.echo(f"Running '{absolute_path}' inferia application...")
     # change cwd to config_path
     os.chdir(absolute_path)
-    if ConfigFile.exists("cogito.yaml"):
-        click.echo("Cogito application is initialized.")
-
-    app = Application(config_file_path=absolute_path)
-    app.run()
+    if not os.path.exists(absolute_path):
+        click.echo(f"Path '{absolute_path}' does not exist.")
+        exit(1)
+    else:
+        # Add the config_path to the sys.path
+        sys.path.insert(0, absolute_path)
+        app = Application(config_file_path=absolute_path)
+        app.run()
