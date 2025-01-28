a43bf777b3944769e59955c7b90a6194da83b365
jamuriano <jamuriano@freepik.com>
2025-01-28 14:04:31 +0100

    refactor(app.py): Remove health check handler and update import statements
    - Delete the import of 'wrap_handler'
    - Update the imports of 'health_check_handler', 'metrics_handler' to include them in the app module
    - Move the health_check_handler function out of the app module, if it exists separately
    - Remove the health check route from the _set_default_routes method

--- inferia/core/app.py
+++ inferia/core/app.py
@@ -10,9 +10,8 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 from inferia.api.handlers import (
-    health_check_handler,
+    health_check_handler, metrics_handler,
 )
-from inferia.core.utils import wrap_handler
 from inferia.api.responses import ErrorResponse
 from inferia.core.config import ConfigFile
 from inferia.core.exceptions import (
@@ -21,7 +20,7 @@ from inferia.core.exceptions import (
 )
 from inferia.core.logging import get_logger
 from inferia.core.models import BasePredictor
-from inferia.core.utils import load_predictor, wrap_handler
+from inferia.core.utils import get_predictor_handler_return_type, load_predictor, wrap_handler
 
 
 class Application:
@@ -29,21 +28,21 @@ class Application:
     ready: bool
 
     def __init__(
-        self,
-        config_file_path: str = ".",
-        logger: Union[Any, logging.Logger] = None,
+            self,
+            config_file_path: str = ".",
+            logger: Union[Any, logging.Logger] = None,
     ):
 
         self._logger = logger or Application._get_default_logger()
 
         try:
             self.config = ConfigFile.load_from_file(
-                os.path.join(f"{config_file_path}/inferia.yaml")
+                    os.path.join(f"{config_file_path}/inferia.yaml")
             )
         except ConfigFileNotFoundError as e:
             self._logger.warning(
-                "config file does not exist. Using default configuration.",
-                extra={"error": str(e), "config_file_path": config_file_path},
+                    "config file does not exist. Using default configuration.",
+                    extra={"error": str(e), "config_file_path": config_file_path},
             )
             self.config = ConfigFile.default()
 
@@ -53,26 +52,22 @@ class Application:
             yield
 
         self.app = FastAPI(
-            title=self.config.inferia.server.name,
-            version=self.config.inferia.server.version,
-            description=self.config.inferia.server.description,
-            access_log=self.config.inferia.server.fastapi.access_log,
-            debug=self.config.inferia.server.fastapi.debug,
-            lifespan=lifespan,
+                title=self.config.inferia.server.name,
+                version=self.config.inferia.server.version,
+                description=self.config.inferia.server.description,
+                access_log=self.config.inferia.server.fastapi.access_log,
+                debug=self.config.inferia.server.fastapi.debug,
+                lifespan=lifespan,
         )
+
+        # FastAPIInstrumentor.instrument_app(self.app, excluded_urls=",".join(
+        #        ["/health-check", "/metrics", "/docs", "/openapi.json"]))
+
         self.app.state.ready = False
 
         self.app.logger = self._logger
 
-        """ Include default routes """
-        self.app.add_api_route(
-            "/health-check",
-            health_check_handler,
-            methods=["GET"],
-            name="health_check",
-            description="Health check endpoint",
-            tags=["health"],
-        )
+        self._set_default_routes()
 
         map_route_to_model: Dict[str, str] = {}
         self.map_model_to_instance: Dict[str, BasePredictor] = {}
@@ -85,66 +80,84 @@ class Application:
                 self.map_model_to_instance[route.predictor] = predictor
             else:
                 self._logger.info(
-                    "Predictor class already loaded",
-                    extra={"predictor": route.predictor},
+                        "Predictor class already loaded",
+                        extra={"predictor": route.predictor},
                 )
 
+            model = self.map_model_to_instance.get(route.predictor)
+            response_model = get_predictor_handler_return_type(model)
+
             handler = wrap_handler(
-                descriptor=route.predictor,
-                original_handler=getattr(
-                    self.map_model_to_instance.get(route.predictor), "predict"
-                ),
+                    descriptor=route.predictor,
+                    original_handler=getattr(
+                            self.map_model_to_instance.get(route.predictor), "predict"
+                    ),
+                    response_model=response_model,
             )
 
             self.app.add_api_route(
-                route.path,
-                handler,
-                methods=["POST"],
-                name=route.name,
-                description=route.description,
-                tags=route.tags,
-                response_model=handler.__annotations__["return"],
-                responses={500: {"model": ErrorResponse}},
+                    route.path,
+                    handler,
+                    methods=["POST"],
+                    name=route.name,
+                    description=route.description,
+                    tags=route.tags,
+                    response_model=response_model,
+                    responses={500: {"model": ErrorResponse}},
             )
 
         async def validation_exception_handler(
-            request: Request, exc: RequestValidationError
+                request: Request, exc: RequestValidationError
         ):
             return JSONResponse(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                content={
-                    "detail": "There is an error with the request parameters.",
-                    "errors": exc.errors(),
-                    "body": exc.body,
-                },
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                    content={
+                        "detail": "There is an error with the request parameters.",
+                        "errors": exc.errors(),
+                        "body": exc.body,
+                    },
             )
 
         self.app.add_exception_handler(
-            RequestValidationError, validation_exception_handler
+                RequestValidationError, validation_exception_handler
         )
+
+    def _set_default_routes(self) -> None:
+        """ Include default routes """
+        self.app.add_api_route(
+                "/health-check",
+                health_check_handler,
+                methods=["GET"],
+                name="health_check",
+                description="Health check endpoint",
+                tags=["health"],
+        )
+
+        self.app.add_api_route("/metrics", metrics_handler, methods=["GET"], name="metrics",
+                               description="Metrics endpoint", tags=["metrics"])
 
     async def setup(self, app: FastAPI):
         self._logger.info("Setting up application", extra={})
         for predictor in self.map_model_to_instance.values():
             try:
                 self._logger.debug(
-                    "Setting up predictor",
-                    extra={"predictor": predictor.__class__.__name__},
+                        "Setting up predictor",
+                        extra={"predictor": predictor.__class__.__name__},
                 )
                 await predictor.setup()
             except Exception as e:
                 self._logger.critical(
-                    "Unable to setting up predictor",
-                    extra={"predictor": predictor.__class__.__name__, "error": e},
+                        "Unable to setting up predictor",
+                        extra={"predictor": predictor.__class__.__name__, "error": e},
                 )
                 raise SetupError(predictor.__class__.__name__, e)
         app.state.ready = True
 
     def run(self):
         uvicorn.run(
-            self.app,
-            host=self.config.inferia.server.fastapi.host,
-            port=self.config.inferia.server.fastapi.port,
+                self.app,
+                host=self.config.inferia.server.fastapi.host,
+                port=self.config.inferia.server.fastapi.port,
         )
 
     @classmethod
