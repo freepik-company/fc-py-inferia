11c400c2dbe46d931af740b50dc730cae325964c
jamuriano <jamuriano@freepik.com>
2025-02-03 09:41:37 +0100

    refactor: Refactor RouteConfig and ServerConfig classes
    Adjusted the structure of RouteConfig and ServerConfig classes to use a single instance instead of a list.
    Updated the initialization of RouteConfig's args attribute to use a list comprehension for better readability.

--- inferia/core/config.py
+++ inferia/core/config.py
@@ -35,22 +35,22 @@ class RouteConfig(BaseModel):
     @classmethod
     def default(cls):
         return cls(
-            name="Predict",
-            description="Make a single prediction",
-            path="/v1/predict",
-            predictor="predict:Predictor",
-            threads=1,
-            args=[
-                ArgConfig(
-                    name="prompt",
-                    type="str",
-                    description="The prompt to generate text from",
-                )
-            ],
-            response=ResponseConfig(
-                type="PredictResponse", description="The generated text"
-            ),
-            tags=["predict"],
+                name="Predict",
+                description="Make a single prediction",
+                path="/v1/predict",
+                predictor="predict:Predictor",
+                threads=1,
+                args=[
+                    ArgConfig(
+                            name="prompt",
+                            type="str",
+                            description="The prompt to generate text from",
+                    )
+                ],
+                response=ResponseConfig(
+                        type="PredictResponse", description="The generated text"
+                ),
+                tags=["predict"],
         )
 
 
@@ -74,18 +74,18 @@ class ServerConfig(BaseModel):
     description: Optional[str] = None
     version: Optional[str] = "1.0.0"
     fastapi: FastAPIConfig
-    routes: List[RouteConfig] = List
+    route: RouteConfig
     cache_dir: str = None
 
     @classmethod
     def default(cls):
         return cls(
-            name="Sapientia per Inferentiam",
-            description="Inference server",
-            version="0.1.0",
-            fastapi=FastAPIConfig.default(),
-            routes=[RouteConfig.default()],
-            cache_dir="/tmp/inferia",
+                name="Sapientia per Inferentiam",
+                description="Inference server",
+                version="0.1.0",
+                fastapi=FastAPIConfig.default(),
+                route=RouteConfig.default(),
+                cache_dir="/tmp/inferia",
         )
 
 
