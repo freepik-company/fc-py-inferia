be9773d51e7f477cf370be50053edbe72ee44065
Sergio Soto Nuñez <ssoto@freepik.com>
2025-02-14 14:49:46 +0100

    feat: adds version command

--- cogito/cli.py
+++ cogito/cli.py
@@ -3,6 +3,7 @@ import click
 from cogito.commands.initialize import init
 from cogito.commands.scaffold_predict import scaffold
 from cogito.commands.run import run
+from cogito.commands.version import version
 
 
 @click.group()
@@ -25,6 +26,7 @@ def cli(ctx, config_path: str = ".") -> None:
 cli.add_command(init)
 cli.add_command(scaffold)
 cli.add_command(run)
+cli.add_command(version)
 
 
 def main():
